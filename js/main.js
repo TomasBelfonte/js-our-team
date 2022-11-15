565b87c52ef904e6467d1ee38d8af1c15cfbcbf9
TomasBelfonte <tomas.belfonte@gmail.com>
2022-11-15 23:29:35 +0100

    JS-OUR-TEAM

--- js/main.js
+++ js/main.js
@@ -10,46 +10,46 @@ const teamMember = [
     nome:"Waine",
     cognome:"Barnett",
     ruolo:"Founder & CEO",
-    img: "wayne-barnett-founder-ceo.jpg"
+    img: "img/wayne-barnett-founder-ceo.jpg"
     },
     {
         nome:"Angela",
         cognome:"Caroll",
         ruolo:"Chief Editor",
-        img: "angela-caroll-chief-editor.jpg"
+        img: "img/angela-caroll-chief-editor.jpg"
     },
     {
         nome:"Walter",
         cognome:"Gordon",
         ruolo:"Office Manager",
-        img: "walter-gordon-office-manager.jpg"
+        img: "img/walter-gordon-office-manager.jpg"
     },
     {
         nome:"Angela",
         cognome:"Lopez",
         ruolo:"Social Media Manager",
-        img: "angela-lopez-social-media-manager.jpg"
+        img: "img/angela-lopez-social-media-manager.jpg"
     },
     {
         nome:"Scott",
         cognome:"Estrada",
         ruolo:"Developer",
-        img: "scott-estrada-developer.jpg"
+        img: "img/scott-estrada-developer.jpg"
     },
     {
         nome:"Barbara",
         cognome:"Ramos",
         ruolo:"Graphic Designer",
-        img: "barbara-ramos-graphic-designer.jpg"
+        img: "img/barbara-ramos-graphic-designer.jpg"
     }
     
 ];
 
 console.log(teamMember);
 
-team1El.innerHTML = teamMember[1].nome + " , " + teamMember[1].cognome + " , " + teamMember[1].ruolo+ " , " + teamMember[1].img;
-team2El.innerHTML = teamMember[2].nome + " , " + teamMember[2].cognome + " , " + teamMember[2].ruolo+ " , " + teamMember[2].img;
-team3El.innerHTML = teamMember[3].nome + " , " + teamMember[3].cognome + " , " + teamMember[3].ruolo+ " , " + teamMember[3].img;
-team4El.innerHTML = teamMember[4].nome + " , " + teamMember[4].cognome + " , " + teamMember[4].ruolo+ " , " + teamMember[4].img;
-team5El.innerHTML = teamMember[5].nome + " , " + teamMember[5].cognome + " , " + teamMember[5].ruolo+ " , " + teamMember[5].img;
-team6El.innerHTML = teamMember[6].nome + " , " + teamMember[6].cognome + " , " + teamMember[6].ruolo+ " , " + teamMember[6].img;
+team1El.innerHTML = teamMember[0].nome + " , " + teamMember[0].cognome + " , " + teamMember[0].ruolo;
+team2El.innerHTML = teamMember[1].nome + " , " + teamMember[1].cognome + " , " + teamMember[1].ruolo;
+team3El.innerHTML = teamMember[2].nome + " , " + teamMember[2].cognome + " , " + teamMember[2].ruolo;
+team4El.innerHTML = teamMember[3].nome + " , " + teamMember[3].cognome + " , " + teamMember[3].ruolo;
+team5El.innerHTML = teamMember[4].nome + " , " + teamMember[4].cognome + " , " + teamMember[4].ruolo;
+team6El.innerHTML = teamMember[5].nome + " , " + teamMember[5].cognome + " , " + teamMember[5].ruolo;
