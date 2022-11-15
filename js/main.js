7fed193a4685c70f4001886ef6585acda6254900
TomasBelfonte <tomas.belfonte@gmail.com>
2022-11-15 22:43:43 +0100

    JS-OUR-TEAM

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
-const teamEl = document.querySelector(".team");
+const team1El = document.querySelector(".team1");
+const team2El = document.querySelector(".team2");
+const team3El = document.querySelector(".team3");
+const team4El = document.querySelector(".team4");
+const team5El = document.querySelector(".team5");
+const team6El = document.querySelector(".team6");
 
 const teamMember = [
     { 
@@ -36,9 +41,15 @@ const teamMember = [
         cognome:"Ramos",
         ruolo:"Graphic Designer",
         img: "barbara-ramos-graphic-designer.jpg"
-    },
+    }
     
 ];
 
 console.log(teamMember);
-teamEl.innerHTML = teamMember.toString;
+
+team1El.innerHTML = teamMember[1].nome + " , " + teamMember[1].cognome + " , " + teamMember[1].ruolo+ " , " + teamMember[1].img;
+team2El.innerHTML = teamMember[2].nome + " , " + teamMember[2].cognome + " , " + teamMember[2].ruolo+ " , " + teamMember[2].img;
+team3El.innerHTML = teamMember[3].nome + " , " + teamMember[3].cognome + " , " + teamMember[3].ruolo+ " , " + teamMember[3].img;
+team4El.innerHTML = teamMember[4].nome + " , " + teamMember[4].cognome + " , " + teamMember[4].ruolo+ " , " + teamMember[4].img;
+team5El.innerHTML = teamMember[5].nome + " , " + teamMember[5].cognome + " , " + teamMember[5].ruolo+ " , " + teamMember[5].img;
+team6El.innerHTML = teamMember[6].nome + " , " + teamMember[6].cognome + " , " + teamMember[6].ruolo+ " , " + teamMember[6].img;
